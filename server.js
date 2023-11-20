78a5b7609d080c1019946d4a48966c493cb44f8b
Your Name <mikael.tallbo@webexpress.se>
2023-11-20 22:32:24 +0100

    Added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const postsRoutes = require('./routes/posts');
 const usersRoutes = require('./routes/users');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
-
 
 
 app.use('/api/posts', postsRoutes);
