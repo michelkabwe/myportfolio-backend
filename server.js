97286dfd4de475c24edd6750293b5efbe4e9a068
Your Name <mikael.tallbo@webexpress.se>
2023-11-30 10:57:38 +0100

    Reconfigured server, now servring  build fodler from client

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const app = express();
-const postsRoutes = require('./routes/posts');
-const usersRoutes = require('./routes/users');
+const path = require('path');
 const cors = require('cors');
 
-app.use(cors());
+const server = express();
 
-app.use(express.json());
+// Set up API routes
+const postsRoutes = require('./routes/posts');
+const usersRoutes = require('./routes/users');
+const categoriesRoutes = require('./routes/categories');
 
+server.use(express.json());
+server.use('/api/posts', postsRoutes);
+server.use('/api/users', usersRoutes);
+server.use('/api/categories', categoriesRoutes);
 
-app.use('/api/posts', postsRoutes);
-app.use('/api/users', usersRoutes);
+server.use(cors());
 
+// Serve static files from the React build directory
+server.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// Handle all other routes by serving the React app's index.html
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
-app.get('/', (req, res) => {
-    res.send("Hello this is the server");
-})
-
-
-
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
