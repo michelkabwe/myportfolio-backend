2462dac8c9c60da960aaf7e25a5eed51dc3d4b80
Your Name <mikael.tallbo@webexpress.se>
2023-12-01 07:39:55 +0100

    Cors options added to dserver.js Removes cors issue in browser

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+
 
 const server = express();
+
+const cors = require('cors');
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET, HEAD, PUT, PATCH, POST , DELETE',
+  creadentials: true
+}
+server.use(cors());
+server.use(cors(corsOptions));
+
+
 
 // Set up API routes
 const postsRoutes = require('./routes/posts');
@@ -14,15 +25,19 @@ server.use('/api/posts', postsRoutes);
 server.use('/api/users', usersRoutes);
 server.use('/api/categories', categoriesRoutes);
 
-server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send('Helloworld');
+})
 
 // Serve static files from the React build directory
-server.use(express.static(path.join(__dirname, '../frontend/build')));
+//server.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Handle all other routes by serving the React app's index.html
-server.get('*', (req, res) => {
+/*server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
+});*/
+
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
