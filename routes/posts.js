813cc3e03d5971395731ca0906aa55045fb9ef1a
Your Name <mikael.tallbo@webexpress.se>
2024-01-02 14:43:23 +0100

    Posts route updated Added imageurl in response

--- routes/posts.js
+++ routes/posts.js
@@ -1,72 +1,71 @@
 const express = require('express');
 const router = express.Router();
-const getDataBase = require('../firebase/database');
-
+const { getDataBase } = require('../firebase/firebase-admin');
 const db = getDataBase();
 
 
 router.get('/', async (req, res) => {
-   try {
-        const posts = []
-
-		const docRef = db.collection('posts');
-		const snapShot = await docRef.get();
-        if(snapShot){
-
-            snapShot.forEach((doc) => {
-                posts.push(doc.data());
-                console.log(posts,'posts');
-            })
-            res.status(200).send(posts);
-        }
-   } catch (error) {
-    console.error("error", error);
-   }
-});
-
-router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-
     try {
+         const posts = []
 
-        const docRef = await db.collection('posts').doc(id).get();
+         const docRef = db.collection('posts');
+         const snapShot = await docRef.get();
+         if(snapShot){
 
-        if (!docRef.exists) {
-            res.status(404).send("Post not found!");
-            return;
-        } else {
-            const userData = docRef.data();
-            const dataWithId = {id: docRef.id, ...userData}
-            res.status(200).send(dataWithId);
-        }
+             snapShot.forEach((doc) => {
+                 posts.push(doc.data());
+             })
+             res.status(200).send(posts);
+         }
     } catch (error) {
-        console.error('Server error, Something went wrong!' + error.message);
-        res.status(500).send(error.message);
+     console.error("error", error);
     }
-})
+ });
 
-router.post('/', async (req, res) => {
-    // Adding post with auto generated id
-   try {
-        const { title, content, selectedCategory } = req.body;
+ router.get('/:id', async (req, res) => {
+     const id = req.params.id;
 
-        await db.collection('posts').add({
-            title: title,
-            content: content,
-            category_id: selectedCategory
+     try {
 
-        });
+         const docRef = await db.collection('posts').doc(id).get();
 
-        res.status(201).json({ message: ' Post created sccessfully'})
+         if (!docRef.exists) {
+             res.status(404).send("Post not found!");
+             return;
+         } else {
+             const userData = docRef.data();
+             const dataWithId = {id: docRef.id, ...userData}
+             res.status(200).send(dataWithId);
+         }
+     } catch (error) {
+         console.error('Server error, Something went wrong!' + error.message);
+         res.status(500).send(error.message);
+     }
+ })
 
-   } catch (error) {
-    console.error("error", error);
-    console.error('Error creating post:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-   }
-});
+ router.post('/', async (req, res) => {
+     // Adding post with auto generated id
+    try {
+         const { title, content, selectedCategory, imageUrl} = req.body;
+
+         await db.collection('posts').add({
+             title: title,
+             content: content,
+             category_id: selectedCategory,
+             imageUrl:imageUrl
+
+         });
+
+         res.status(201).json({ message: ' Post created sccessfully'})
+
+    } catch (error) {
+     console.error("error", error);
+     console.error('Error creating post:', error);
+     res.status(500).json({ error: 'Internal Server Error' });
+    }
+ });
 
 
 
 
-module.exports = router;
+ module.exports = router;
