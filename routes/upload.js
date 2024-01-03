2cc652c6d2a126403415c20425700cc14eb05c23
Your Name <mikael.tallbo@webexpress.se>
2024-01-03 21:12:59 +0100

    Upload route modified and fixed

--- routes/upload.js
+++ routes/upload.js
@@ -3,55 +3,49 @@ const router = express.Router();
 const multer = require('multer');
 const admin = require('firebase-admin');
 
+const bucket = admin.storage().bucket();
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.post('/', upload.single('file'), async (req, res) => {
   try {
-    const uploadedFile = req.file;
-    if (!uploadedFile) {
-      return res.status(400).json({ error: 'No file uploaded.' });
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
     }
 
-    const bucket = admin.storage().bucket();
-    const fileRef = bucket.file(`images/${uploadedFile.originalname}`);
+    const uploadedFile = req.file;
+    const destination = bucket.file(`images/${uploadedFile.originalname}`);
 
-    await fileRef.setMetadata({ contentType: uploadedFile.mimetype });
-
-    const fileStream = fileRef.createWriteStream({
+    // Ensure you're passing the file buffer directly to the Firebase bucket upload
+    await destination.save(uploadedFile.buffer, {
       metadata: {
-        contentType: uploadedFile.mimetype
-      }
+        contentType: uploadedFile.mimetype,
+      },
     });
 
-    fileStream.on('error', (error) => {
-      console.error('Error uploading file:', error);
-      res.status(500).send('Internal Server Error');
+    const expirationDate = new Date(Date.now() + 3600000);
+
+    // Get the download URL for the uploaded file
+    const [downloadUrl] = await destination.getSignedUrl({
+      action: 'read',
+      expires: expirationDate,
+
+
     });
 
-    fileStream.on('finish', async () => {
-      try {
-        // Get the download URL for the uploaded file
-        const [downloadUrl] = await fileRef.getSignedUrl({
-          action: 'read'
-          //expires: '01-01-2025' // Set an expiration date for the URL if needed
-        });
-
-        res.status(200).json({
-          message: 'File uploaded successfully',
-          imageUrl: downloadUrl
-        });
-      } catch (error) {
-        console.error('Error getting download URL:', error);
-        res.status(500).send('Internal Server Error');
-      }
+    res.status(200).json({
+      message: 'File uploaded successfully',
+      imageUrl: downloadUrl
     });
 
-    fileStream.end(uploadedFile.buffer);
+    // Send a success response
   } catch (error) {
+    // Log the error for debugging purposes
     console.error('Error uploading file:', error);
-    res.status(500).send('Internal Server Error');
+    return res.status(500).send('Error uploading file.');
   }
 });
+
 
 module.exports = router;
